953ede1d1d9d32b412f668fec5951f208f3ff860
e3mkavya <kavyam@thoughtworks.com>
2021-01-21 15:35:42 +0530

    [Kavya | Srini]  remove protractor, use jest instead of karma and jasmine

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,25 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1Page } from './tab1.page';
+import {render} from '@testing-library/angular';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('Tab1Page', () => {
-  let component: Tab1Page;
-  let fixture: ComponentFixture<Tab1Page>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [Tab1Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(Tab1Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
+  it('should render tab1 component', async () => {
+    const component = await render(Tab1Page, {
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
     expect(component).toBeTruthy();
   });
 });
