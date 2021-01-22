c326d506d96766d6ea2c6ecf2b7c111a6d9b91de
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-01-22 14:48:10 +0530

    add component test for ionic components

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,6 +1,7 @@
 import { Tab1Page } from './tab1.page';
-import {render} from '@testing-library/angular';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { render, screen } from '@testing-library/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { log } from 'console';
 
 describe('Tab1Page', () => {
   it('should render tab1 component', async () => {
@@ -8,5 +9,13 @@ describe('Tab1Page', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to check camera component', async () => {
+    await render(Tab1Page, {
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+
+    expect(screen.getByTitle(/camera/i)).toBeTruthy();
   });
 });
